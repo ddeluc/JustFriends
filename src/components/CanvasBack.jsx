c70781ed5a71224fed36b29027567ac70f597981
dante deluca <dantedeluca3219@icloud.com>
2024-10-30 14:29:40 -0400

    Canvas Front Back

---
+++ src/components/CanvasBack.jsx
@@ -0,0 +1,88 @@
+import { motion, transform } from "framer-motion";
+
+import { styles } from "../styles";
+import { demomode } from "../constants";
+
+import akiraClip1 from '../assets/videos/Akira1.mp4';
+import champlooClip1 from '../assets/videos/SamuraiChamploo1.mp4';
+import oceanWaves1 from '../assets/videos/oceanWaves1.mp4';
+import whiteNoise from '../assets/videos/WhiteNoise/whiteNoise.mp4'
+import kuroko1 from '../assets/videos/Kuroko1.mp4';
+import cowboy1 from '../assets/videos/Cowboy1.mp4';
+import cityTrain from '../assets/videos/CityTrain.mp4';
+import farine from '../assets/videos/farinefiveroses.mp4';
+
+
+const demobox = demomode ? styles.demo.landing : {};
+
+const CanvasBack = ({ hoveredItem, selectedItem }) => {
+
+  const handleVideoEnd = () => {    
+    setCurrentVideoIndex((currentVideoIndex + 1)%videoSources.length);    
+  };
+
+  return (
+    <div style={demobox} className={`absolute inset-0 max-h-172 max-w-5xl mx-auto grid grid-cols-3 gap-5 my-8`}>
+      <div style={demobox} className={`relative text-yellow-300 text-center text-[45px] font-shrikhand`}>
+        {hoveredItem == "About" ?
+          <video
+            style={demobox}
+            className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-85`}
+            src={akiraClip1} onEnded={handleVideoEnd} autoPlay muted loop
+          />
+          : 
+          <video
+            style={demobox}
+            className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-50 grayscale`}
+            src={akiraClip1} onEnded={handleVideoEnd} autoPlay muted loop
+          />
+        }               
+      </div >
+      <div style={demobox} className={`relative text-yellow-300 text-center text-[45px] font-shrikhand`}>
+        {selectedItem == "Volumes" ? 
+          <>
+            <video
+              style={demobox}
+              className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-85`}
+              src={champlooClip1} onEnded={handleVideoEnd} autoPlay muted loop
+            />
+          </>
+        :
+          <>
+            {hoveredItem == "Volumes" ?
+              <video
+                style={demobox}
+                className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-85`}
+                src={champlooClip1} onEnded={handleVideoEnd} autoPlay muted loop
+              />
+              : 
+              <video
+                style={demobox}
+                className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-50 grayscale`}
+                src={champlooClip1} onEnded={handleVideoEnd} autoPlay muted loop
+              />
+            }    
+          </>
+        }
+          
+      </div>
+      <div style={demobox} className={`relative text-yellow-300 text-center text-[45px] font-shrikhand`}>
+        {hoveredItem == "Join" ?
+          <video
+            style={demobox}
+            className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-85`}
+            src={cityTrain} onEnded={handleVideoEnd} autoPlay muted loop
+          />
+          : 
+          <video
+            style={demobox}
+            className={`top-0 left-0 w-full h-full absolute object-none rounded-3xl brightness-50 grayscale`}
+            src={cityTrain} onEnded={handleVideoEnd} autoPlay muted loop
+          />
+        }
+      </div>                       
+    </div>
+  )
+}
+
+export default CanvasBack;
---
+++ src/components/Volumes.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+
+import { demomode } from "../constants";
+import { aboutText } from "../constants";
+
+import akiraClip1 from '../assets/videos/Akira1.mp4';
+import champlooClip1 from '../assets/videos/SamuraiChamploo1.mp4';
+import oceanWaves1 from '../assets/videos/oceanWaves1.mp4';
+import whiteNoise from '../assets/videos/WhiteNoise/whiteNoise.mp4'
+import kuroko1 from '../assets/videos/Kuroko1.mp4';
+import cowboy1 from '../assets/videos/Cowboy1.mp4';
+import cityTrain from '../assets/videos/CityTrain.mp4';
+import farine from '../assets/videos/farinefiveroses.mp4';
+
+const demobox = demomode ? styles.demo.landing : {};
+
+const Volumes = ({ hoveredItem, selectedItem }) => {
+  useEffect(() => {
+    console.log(hoveredItem);
+    console.log(selectedItem);
+    console.log("rendered about")
+  }, []);
+
+  return (
+    <>
+      <div style={demobox} className={`absolute flex justify-end inset-0 max-h-172 max-w-5xl mx-auto gap-5 my-12`}>      
+             
+      </div>
+    </>    
+  );
+};
+
+export default Volumes;
